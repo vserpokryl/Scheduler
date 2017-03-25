2cbb097ad574b7d8aaf4d645046fddee4aa95d99
Vasiliy Serpokryl <nanografon@mail.ru>
2017-03-26 00:50:56 +0700

    Testing fix

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -31,7 +31,7 @@ abstract class DuskTestCase extends BaseTestCase
     protected function driver()
     {
         return RemoteWebDriver::create(
-            'http://localhost:9515', DesiredCapabilities::chrome()
+            'http://localhost:9515', DesiredCapabilities::phantomjs()
         );
     }
 }
